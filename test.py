c92b80ae468f032c573e8b6bc19e48fd7e16f0a1
Sasidharan Mahalingam <sasim1094@gmail.com>
2025-06-07 15:56:42 -0700

    Refactoring the train and test python sripts + adding command line argument parser for both scripts

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# Python script that run the training model
+# Python script that runs the trained model
 # Author: Sasidhran Mahalingam
 # Date Created: May 7 2025
 
@@ -11,99 +11,181 @@ import torchvision.transforms.functional as TF
 from torchvision.utils import save_image
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
-CHECKPOINT = "./weights/checkpoint_epoch100.pth"
-INPUT_DIR = "./test_data/RANDOM_IMG_1"
-OUTPUT_DIR = "./results"
-os.makedirs(OUTPUT_DIR, exist_ok=True)
+def parse_args():
+    """
+    Function that parses the input arguments
 
-# Configuration
-lfsize    = (432, 620, 9, 9)        # replace V, U with your angular dims
-disp_mult = 4.0                  # same as training
-device    = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    Parameters:
+    -----------
+    None
 
-# Load model
-model = ForwardModel(lfsize=lfsize, disp_mult=disp_mult).to(device)
-checkpoint = torch.load(CHECKPOINT, map_location=device)
-model.load_state_dict(checkpoint["model_state"])
-model.eval()
+    Returns:
+    --------
+    Returns the parsed argmuent values
+    """
+    parser = argparse.ArgumentParser(
+        description=(
+        "Python script for tesing the CNN framework to synthesize Light Field data from a single RGB image\n"
+        "The script needs the path to the pre-trained weights, path to the input image and the path to the output directory for saving the synthesized light fields\n"
+        "All other parameters are optional.\n\n"
+        
+        "Example:\n"
+        "    python test.py --pretrained_weights <path to the pre-trained weights> --input_image <path to the input iamge> --output_path <path to the output directory>\n"
+        ),
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument('--pretrained_weights', type=str, required=True,
+                        help='Path to the pre-trained weights.')
+    parser.add_argument('--input_image', type=str, required=True,
+                        help='Path to the input image for which the light field has to be synthesized.')
+    parser.add_argument('--output_path', type=str, required=True,
+                        help='Path to the save the synthesized light fields.')
+    parser.add_argument('--save_ray_depths', type=bool,required=False, default=False,
+                        help='Flag to enable saving of the ray depths.')
+    parser.add_argument('--save_lambertian', type=bool,required=False, default=False,
+                        help='Flag to enable saving of the lambertian light fields.')
+    parser.add_argument('--lfsize', type=tuple,required=False, default=(432, 620, 9, 9),
+                        help='Tuple that defines the lize of the generated light field.')
+    parser.add_argument('--disp_mult', type=float, required=False, default=(4.0),
+                        help='Float disp_mult that defines the disparity range (-disp_mult, disp_mult).')
+    return parser.parse_args()
 
-# Prepare a batch of center views (aif_batch), e.g. from a DataLoader or single image
-img = Image.open(os.path.join(INPUT_DIR, 'view_06_06.png')).convert("RGB")
-img_t = TF.to_tensor(img).float().unsqueeze(0)    # [1,3,H,W], range [0,1]
-# if model expects [-1,1] range:
-img_t = img_t * 2.0 - 1.0                
-aif_batch = img_t.to(device)
 
-# Infer
-with torch.no_grad():
-    ray_depths, lf_shear, y = model(aif_batch)
-    # lf_shear: [1, H, W, V, U, 3], in [-1,1] if model output normalized
+def test(weights, input_image, output_dir, save_ray_depths=False, save_lambertian=False,
+         lfsize=(432, 620, 9, 9), disp_mult=4.0):
+    """
+    Function that does the inference of the model to generate the light field data
+    
+    Parameters:
+    -----------
+    weights         - String that holds the path to the pre-trained weights
+    input_image     - String that holds the path to the input image
+    output_dir      - String that holds the path to save the generate light fields
+    save_ray_depths - Flag to enable/diable the saving of the ray depths
+    save_lambertian - Flag to enable/disable the saving of the lambertian light fields
+    lfsize          - Tuple that defines the dimensions of the light field
+    disp_mult       - Floating point value that defines the range of valid disparities
 
-# Save each sub-aperture view
-_, H, W, V, U, C = y.shape
-# Bring to [V*U, C, H, W] and [0,1] range
-views = y.squeeze(0)        # [H,W,V,U,3]
-views = views.permute(2, 3, 4, 0, 1)  # [V,U,3,H,W]
-views = views.reshape(V*U, C, H, W)
+    Returns:
+    --------
+    Nothing
+    """
+    os.makedirs(output_dir, exist_ok=True)
 
-# images are in [-1,1], map back to [0,1]
-views = (views + 1.0) * 0.5
-views = views.clamp(0,1)
+    device    = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Save the lambertian light fields
-lambertian_views = lf_shear.squeeze(0)        # [H,W,V,U,3]
-lambertian_views = lambertian_views.permute(2, 3, 4, 0, 1)  # [V,U,3,H,W]
-lambertian_views = lambertian_views.reshape(V*U, C, H, W)
+    # Load model
+    model = ForwardModel(lfsize=lfsize, disp_mult=disp_mult).to(device)
+    checkpoint = torch.load(weights, map_location=device)
+    model.load_state_dict(checkpoint["model_state"])
+    model.eval()
 
-# images are in [-1,1], map back to [0,1]
-lambertian_views = (views + 1.0) * 0.5
-lambertian_views = views.clamp(0,1)
+    # Prepare a batch of center views (aif_batch), e.g. from a DataLoader or single image
+    img = Image.open(input_image).convert("RGB")
+    img_t = TF.to_tensor(img).float().unsqueeze(0)    # [1,3,H,W], range [0,1]
+    # if model expects [-1,1] range:
+    img_t = img_t * 2.0 - 1.0                
+    aif_batch = img_t.to(device)
 
-# Get the disaprities, convert them into depths and normalize them
-depths = ray_depths.squeeze(0)
-depths = depths.permute(2, 3, 0, 1)
-depths = depths.reshape(V*U, H, W)
-depths = depths.cpu().detach().numpy()
-disp_min, disp_max = depths.min(), depths.max()
-# Shift the results to avoid negative values
-depths = depths - disp_min
-# Avoiding divide by zero
-depths[depths == 0] = 1e-6
-disp_min, disp_max = depths.min(), depths.max()
-# Calculate the depths from the disparity
-depths = (3000 * 0.05) / depths
-# Clip the values from 18.75 to 50
-vmin = 18.75
-vmax = 50
-depths = np.clip(depths, vmin, vmax)
-# Taking log of the values for better visualization
-depth_log = np.log10(depths)
-depth_log_norm = (depth_log - depth_log.min()) / (depth_log.max() - depth_log.min())
+    # Infer
+    with torch.no_grad():
+        ray_depths, lf_shear, y = model(aif_batch)
+        # lf_shear: [1, H, W, V, U, 3], in [-1,1] if model output normalized
 
-# save grid or individual files
-for idx in range(V*U):
-    v = idx // U
-    u = idx % U
-    out_path = os.path.join(OUTPUT_DIR, 'sub-aperture_images',f"view_{v}_{u}.png")
-    save_image(views[idx], out_path)
+    # Save each sub-aperture view
+    _, H, W, V, U, C = y.shape
+    # Bring to [V*U, C, H, W] and [0,1] range
+    views = y.squeeze(0)        # [H,W,V,U,3]
+    views = views.permute(2, 3, 4, 0, 1)  # [V,U,3,H,W]
+    views = views.reshape(V*U, C, H, W)
 
-# save grid or individual files
-for idx in range(V*U):
-    v = idx // U
-    u = idx % U
-    out_path = os.path.join(OUTPUT_DIR, 'lambertian_sub-aperture_images',f"view_{v}_{u}.png")
-    save_image(lambertian_views[idx], out_path)
+    # images are in [-1,1], map back to [0,1]
+    views = (views + 1.0) * 0.5
+    views = views.clamp(0,1)
 
-# save estimated depths
-for idx in range(V*U):
-    v = idx // U
-    u = idx % U
-    out_path = os.path.join(OUTPUT_DIR, 'estimated_disparity', f"view_{v}_{u}.png")
-    colormap = plt.get_cmap('plasma')
-    colored_disp = colormap(depth_log_norm[idx])
-    colored_disp = (colored_disp[:, :, :3] * 255).astype(np.uint8)
-    depth_image = Image.fromarray(colored_disp)
-    depth_image.save(out_path)
+    # save grid or individual files
+    for idx in range(V*U):
+        v = idx // U
+        u = idx % U
+        out_path = os.path.join(output_dir, 'sub-aperture_images')
+        os.makedirs(out_path, exist_ok=True)
+        out_path = os.path.join(output_dir, 'sub-aperture_images',f"view_{v}_{u}.png")
+        save_image(views[idx], out_path)
 
-print("Saved", V*U, "views to", OUTPUT_DIR)
+    if save_lambertian:
+        # Save the lambertian light fields
+        lambertian_views = lf_shear.squeeze(0)        # [H,W,V,U,3]
+        lambertian_views = lambertian_views.permute(2, 3, 4, 0, 1)  # [V,U,3,H,W]
+        lambertian_views = lambertian_views.reshape(V*U, C, H, W)
+
+        # images are in [-1,1], map back to [0,1]
+        lambertian_views = (views + 1.0) * 0.5
+        lambertian_views = views.clamp(0,1)
+
+        # save grid or individual files
+        for idx in range(V*U):
+            v = idx // U
+            u = idx % U
+            out_path = os.path.join(output_dir, 'lambertian_sub-aperture_images')
+            os.makedirs(out_path, exist_ok=True)
+            out_path = os.path.join(output_dir, 'lambertian_sub-aperture_images',f"view_{v}_{u}.png")
+            save_image(lambertian_views[idx], out_path)
+
+    if save_ray_depths:
+        # Get the disaprities, convert them into depths and normalize them
+        depths = ray_depths.squeeze(0)
+        depths = depths.permute(2, 3, 0, 1)
+        depths = depths.reshape(V*U, H, W)
+        depths = depths.cpu().detach().numpy()
+        disp_min, disp_max = depths.min(), depths.max()
+        # Shift the results to avoid negative values
+        depths = depths - disp_min
+        # Avoiding divide by zero
+        depths[depths == 0] = 1e-6
+        disp_min, disp_max = depths.min(), depths.max()
+        # Calculate the depths from the disparity
+        depths = (3000 * 0.05) / depths
+        # Clip the values from 18.75 to 50. Might have to change the values for better visuzlization
+        vmin = 18.75
+        vmax = 50
+        depths = np.clip(depths, vmin, vmax)
+        # Taking log of the values for better visualization
+        depth_log = np.log10(depths)
+        depth_log_norm = (depth_log - depth_log.min()) / (depth_log.max() - depth_log.min())
+        # save estimated depths
+        for idx in range(V*U):
+            v = idx // U
+            u = idx % U
+            out_path = os.path.join(output_dir, 'estimated_disparity')
+            os.makedirs(out_path, exist_ok=True)
+            out_path = os.path.join(output_dir, 'estimated_disparity', f"view_{v}_{u}.png")
+            colormap = plt.get_cmap('plasma')
+            colored_disp = colormap(depth_log_norm[idx])
+            colored_disp = (colored_disp[:, :, :3] * 255).astype(np.uint8)
+            depth_image = Image.fromarray(colored_disp)
+            depth_image.save(out_path)
+
+def main():
+    """
+    Driver function for training the model
+
+    Parameters:
+    -----------
+    None
+
+    Returns:
+    --------
+    Nothing
+    """
+    args = parse_args()
+    print('Starting Inference...')
+    test(args.pretrained_weights, args.input_image, args.output_path, 
+         save_ray_depths=args.save_ray_depths, save_lambertian=args.save_lambertian,
+         lfsize=args.lfsize, disp_mult=args.disp_mult)
+    print('Inference done. Synthesized Light Field data saved.')
+
+# Run the driver function
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -10,89 +10,175 @@ from losses.losses import tvLoss, depthConsistencyLoss
 import torch.optim as optim
 import os
 from tqdm import tqdm
+import argparse
 
-# training_data_path              = "/home/sasidharan/Projects/Plenoptic Camera/Datasets/Flower Dataset/Sub-Aperture Images/Train"
-training_data_path              = "/home/sasidharan/Projects/Plenoptic Camera/Datasets/Test Dataset"
-weights_save_path               = "/home/sasidharan/Projects/Plenoptic Camera/Code/Learning-to-Synthesize-a-4D-RGBD-Light-Field-from-a-Single-Image/weights"
-checkpoint_path                 = ""
-grid                            = (13, 13)                # U×V views
-crop_size                       = (256, 256)              # spatial-crop height & width
-batch_size                      = 2
-epochs                          = 1000
-learning_rate                   = 1e-4
-disp_mult                       = 4.0
-lam_tv                          = 0.01
-lam_dc                          = 0.005
-lfsize                          = (432,622,9,9)
+def parse_args():
+    """
+    Function that parses the input arguments
 
-loader = getDataloader(
-    training_data_path, grid, spatial_crop=crop_size,
-    batch_size=batch_size,
-    resize=None,               
-    num_workers=4
-)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    Parameters:
+    -----------
+    None
 
-model = ForwardModel(lfsize=lfsize, disp_mult=disp_mult).to(device)
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    Returns:
+    --------
+    Returns the parsed argmuent values
+    """
+    parser = argparse.ArgumentParser(
+        description=(
+        "Python script for training the CNN framework to synthesize Light Field data from a single RGB image\n"
+        "The script needs the path to the training data and the path for saving the weights\n"
+        "All other parameters are optional.\n\n"
+        
+        "Example:\n"
+        "    python train.py --training_data_path <path to directory containing the light data for training> --weights_save_path <path to directory where the weights have to be saved>\n"
+        ),
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument('--training_data_path', type=str, required=True,
+                        help='Path to the training data.')
+    parser.add_argument('--weights_save_path', type=str, required=True,
+                        help='Path where model weights should be saved.')
+    parser.add_argument('--checkpoint_path', type=str, required=False, default="",
+                        help='Path where model weights read from to resume training.')
+    parser.add_argument('--grid', type=tuple, required=False, default=(13,13),
+                        help='Tuple (U,V) that define the angular dimensions of the Light Field data.')
+    parser.add_argument('--crop_size', type=tuple, required=False, default=(256,256),
+                        help='Tuple (H,W) setting for the size of the random crop used for training.')   
+    parser.add_argument('--batch_size', type=int, required=False, default=2,
+                        help='Batch size used for training.')
+    parser.add_argument('--epochs', type=int, required=False, default=1000,
+                        help='Number of epochs to train for.')
+    parser.add_argument('--learning_rate', type=float, required=False, default=1e-4,
+                        help='Learning rate used for training.')
+    parser.add_argument('--disp_mult', type=float, required=False, default=(4.0),
+                        help='Float disp_mult that defines the disparity range (-disp_mult, disp_mult).')
+    parser.add_argument('--lam_tv', type=float, required=False, default=0.01,
+                        help='Weight for the TV loss used for training.')
+    parser.add_argument('--lam_dc', type=float, required=False, default=0.005,
+                        help='Weight for the DC loss used for training.')
+    parser.add_argument('--lfsize', type=tuple, required=False, default=(432,622,9,9),
+                        help='Tuple (H,W,U,V) that defines the deimensions of the captured Light Field data.')
+    return parser.parse_args()
 
-checkpoint = None
 
-if checkpoint_path != "" and os.path.exists(checkpoint_path):
-    print("Loading model state from checkpoint...")
-    checkpoint = torch.load(checkpoint_path)
-    model.load_state_dict(checkpoint["model_state"])
-    optimizer.load_state_dict(checkpoint["optim_state"])
 
-for epoch in range(epochs):
-    model.train()
-    total_loss = 0.0
-    loop = tqdm(enumerate(loader), total=len(loader), ncols=80, desc=f"Epoch [{epoch+1}/{epochs}]", leave=True, dynamic_ncols=False)
-    for i, (aif_batch, lf_batch) in loop:
-        # move to device
-        aif_batch = aif_batch.to(device)          # [B,3,h,w]
-        lf_batch  = lf_batch.to(device)           # [B,h,w,V,U,3]
 
-        # forward pass
-        ray_depths, lf_shear, y = model(aif_batch)
-        # ray_depths: [B,h,w,V,U]
-        # lf_shear:   [B,h,w,V,U,3]
-        # y:          [B,h,w,V,U,3]
+def train(training_data_path, weights_save_path, checkpoint_path="", grid=(13,13), 
+          crop_size=(256,256), batch_size=2, epochs=1000, learning_rate=1e-4, disp_mult=4.0,
+          lam_tv=0.01, lam_dc=0.005, lfsize=(432,622,9,9)):
+    """
+    Function that does the CNN framework training
 
-        # 1) shear reconstruction loss
-        shear_loss = torch.mean(torch.abs(lf_shear - lf_batch))
+    Arguments:
+    ----------
+    training_data_path  - String that holds the path to the directory containing the sub-aperture views
+    weights_save_path   - String that holds the path to the directory where the weights should be saved
+    checkpoint_path     - String that holds the path to the stored weights if training has to be resumed
+    grid                - Tuple that defines the U,V angular dimensions of the light field
+    crop_size           - Tuple that defines the size of the random crop used for training
+    batch_size          - Integer values that defines the batch size of the training pipeline
+    epochs              - Integer that defines the number of epochs
+    learning_rate       - Floating point value that defines the learning rate used for training
+    disp_mult           - Floating point value that defines the range of valid disparities
+    lam_tv              - Floating point value that defines the Total Variational Loss weightage used for training
+    lam_dc              - Floating value that defines the Consistency Regularization Loss weightage used for training
+    lfsize              - Tuple that defines the dimensions of the 4D Light Field
 
-        # 2) occlusion‐corrected output loss
-        output_loss = torch.mean(torch.abs(y - lf_batch))
+    """
 
-        # 3) TV on depth
-        tv = tvLoss(ray_depths, lfsize)
+    loader = getDataloader(
+        training_data_path, grid, spatial_crop=crop_size,
+        batch_size=batch_size,
+        resize=None,               
+        num_workers=4
+    )
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        # 4) depth consistency
-        dc = depthConsistencyLoss(ray_depths, lfsize)
+    model = ForwardModel(lfsize=lfsize, disp_mult=disp_mult).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-        # total
-        loss = shear_loss + output_loss + lam_tv * tv + lam_dc * dc
+    checkpoint = None
 
-        # backward + step
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    if checkpoint_path != "" and os.path.exists(checkpoint_path):
+        print("Loading model state from checkpoint...")
+        checkpoint = torch.load(checkpoint_path)
+        model.load_state_dict(checkpoint["model_state"])
+        optimizer.load_state_dict(checkpoint["optim_state"])
 
-        total_loss += loss.item() * aif_batch.size(0)
+    for epoch in range(epochs):
+        model.train()
+        total_loss = 0.0
+        loop = tqdm(enumerate(loader), total=len(loader), ncols=80, desc=f"Epoch [{epoch+1}/{epochs}]", leave=True, dynamic_ncols=False)
+        for i, (aif_batch, lf_batch) in loop:
+            # move to device
+            aif_batch = aif_batch.to(device)          # [B,3,h,w]
+            lf_batch  = lf_batch.to(device)           # [B,h,w,V,U,3]
 
-    avg_loss = total_loss / len(loader.dataset)
-    
-    if(((epoch + 1) % 1 == 0) or epoch == 0):
-        print(f"Epoch {epoch+1}/{epochs}, Loss: {avg_loss:.4f}")
+            # forward pass
+            ray_depths, lf_shear, y = model(aif_batch)
+            # ray_depths: [B,h,w,V,U]
+            # lf_shear:   [B,h,w,V,U,3]
+            # y:          [B,h,w,V,U,3]
 
-    if(((epoch + 1) % 10 == 0) and epoch > 1):
-        ckpt = {
-            "epoch": epoch + 1,
-            "model_state": model.state_dict(),
-            "optim_state": optimizer.state_dict(),
-            "loss": avg_loss,
-        }
-        weights_name = f"checkpoint_epoch{epoch+1}.pth"
-        torch.save(ckpt, os.path.join(weights_save_path, weights_name))
-torch.save(model.state_dict(), os.path.join(weights_save_path, "forward_model_final.pth"))
+            # 1) shear reconstruction loss
+            shear_loss = torch.mean(torch.abs(lf_shear - lf_batch))
+
+            # 2) occlusion‐corrected output loss
+            output_loss = torch.mean(torch.abs(y - lf_batch))
+
+            # 3) TV on depth
+            tv = tvLoss(ray_depths, lfsize)
+
+            # 4) depth consistency
+            dc = depthConsistencyLoss(ray_depths, lfsize)
+
+            # total
+            loss = shear_loss + output_loss + lam_tv * tv + lam_dc * dc
+
+            # backward + step
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item() * aif_batch.size(0)
+
+        avg_loss = total_loss / len(loader.dataset)
+        
+        if(((epoch + 1) % 1 == 0) or epoch == 0):
+            print(f"Epoch {epoch+1}/{epochs}, Loss: {avg_loss:.4f}")
+
+        if(((epoch + 1) % 10 == 0) and epoch > 1):
+            ckpt = {
+                "epoch": epoch + 1,
+                "model_state": model.state_dict(),
+                "optim_state": optimizer.state_dict(),
+                "loss": avg_loss,
+            }
+            weights_name = f"checkpoint_epoch{epoch+1}.pth"
+            torch.save(ckpt, os.path.join(weights_save_path, weights_name))
+    torch.save(model.state_dict(), os.path.join(weights_save_path, "forward_model_final.pth"))
+
+def main():
+    """
+    Driver function for training the model
+
+    Parameters:
+    -----------
+    None
+
+    Returns:
+    --------
+    Nothing
+    """
+    args = parse_args()
+    print("Starting training...")
+    train(args.training_data_path, args.weights_save_path, checkpoint_path=args.checkpoint_path, 
+          grid=args.grid, crop_size=args.crop_size, batch_size=args.batch_size,
+          epochs=args.epochs, learning_rate=args.learning_rate, disp_mult=args.disp_mult,
+          lam_tv=args.lam_tv, lam_dc=args.lam_dc, lfsize=args.lfsize)
+    print('Training Done.')
+
+# Run the driver function
+if __name__ == "__main__":
+    main()
